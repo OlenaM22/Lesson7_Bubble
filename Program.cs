92e964ef396cb3c802159526c7d611656f1fdfdf
OlenaM22 <143819929+OlenaM22@users.noreply.github.com>
2023-10-05 20:50:41 +0300

    Lesson7bubble

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson7
+{
+    public class Program
+    {
+        public static void BubbleSort(int[] arr)
+        {
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
